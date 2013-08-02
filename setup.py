1ec2f2b84db86b59a26b0af2c8cb6a51c8f7bfbd
Mike Thornton <msthornton@gmail.com>
2013-08-02 07:52:12 -0500

    Create setup.py
    Setup.py please

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+VERSION = "1.02"
+
+setup(
+name = "PySocks",
+version = VERSION,
+description = "A Python SOCKS module",
+url = "https://github.com/six8/PySocks",
+license = "BSD",
+py_modules=["socks"]
+)
